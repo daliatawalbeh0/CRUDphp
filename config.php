9a7163242af6e03f64ade3887be74112c4f6b645
daliatawalbeh0 <157593586+daliatawalbeh0@users.noreply.github.com>
2024-07-21 16:21:41 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "Dalia";
+$password = "D123";
+$dbname = "dalia's form";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
